c827e6a268b9f555ff0c76cb5a8c9e4be3f59168
jojo-2605 <jolanmayeur@gmail.com>
2026-08-27 14:12:34 +0200

    edit runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  runtimeConfig: {
-    dbHost: "77.37.35.163",
+   runtimeConfig: {
+    dbHost: '77.37.35.163', //srv660.hstgr.io
     dbPort: 3306,
-    dbUser: "u868520261_nuxt",
-    dbName: "u868520261_nuxt",
-    dbPassword: "nuxtCepegra11",
+    dbUser: 'u868520261_nuxt',
+    dbName:'u868520261_nuxt',
+    dbPassword: 'NuxtCepegra11!'
+    // Clé accessible uniquement côté serveur
+    //apiSecretToken: process.env.API_SECRET_TOKEN || 'mon-super-token-secret-123'
   }
 })
--- server/api/moto/index.js
+++ server/api/moto/index.js
@@ -1,11 +1,10 @@
-export default defineEventHandler(async(event) => {
+export default defineEventHandler(async (event) => {
     try {
-      const [rows] = await db.query('SELECT * FROM moto')
-      return rows
+        const [rows] = await db.query('SELECT * FROM motos')
+        return rows
     }catch(error) {
-      throw createError({
-        statusCode: 500,
-        statusMessage: `Erreur MySQL: ${error.message}`
-      })
+        throw createError({
+            statusCode: 500,
+            statusMessage: `Erreur MySQL: ${error.message}`        })
     }
 })
