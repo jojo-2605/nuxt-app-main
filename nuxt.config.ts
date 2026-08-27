ccf515b63ef9e34da74cfb291f9bb2a715c96e6c
jojo-2605 <jolanmayeur@gmail.com>
2026-08-27 14:08:13 +0200

    edit host

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   runtimeConfig: {
-    dbHost: "srv660.hstgr.io",
+    dbHost: "77.37.35.163",
     dbPort: 3306,
     dbUser: "u868520261_nuxt",
     dbName: "u868520261_nuxt",
