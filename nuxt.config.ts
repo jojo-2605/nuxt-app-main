3547fbaa3bde4b99fbd5f9b303cef8218c65f28b
jojo-2605 <jolanmayeur@gmail.com>
2026-08-27 13:59:29 +0200

    add mysql

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    dbHost: "srv660.hstgr.io",
+    dbPort: 3306,
+    dbUser: "u868520261_nuxt",
+    dbName: "u868520261_nuxt",
+    dbPassword: "nuxtCepegra11",
+  }
 })
